3a23cdc04e64652875b09373e8593d1b080af436
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2024-05-29 22:27:27 +0300

    Modified transaction view and transaction widget

--- uiWallet/tab/widget/transaction.py
+++ uiWallet/tab/widget/transaction.py
@@ -11,9 +11,10 @@ from svc.blockchain.src.model import (
 
 
 class TransactionWidget(QWidget):
-    def __init__(self, transaction: Transaction, parent: QObject | None = None) -> None:
+    def __init__(self, transaction: Transaction, incoming: bool, parent: QObject | None = None) -> None:
         super(TransactionWidget, self).__init__(parent)
         self.transaction = transaction
+        self.incoming: bool = incoming
         self._setup_layout()
         
     def _setup_layout(self) -> None:
@@ -21,8 +22,10 @@ class TransactionWidget(QWidget):
         layout.addWidget(
             QLabel('To address: %s' % self.transaction.toAddress)
         )
+        amount_label = QLabel('Amount: %f' % self.transaction.amount)
+        amount_label.setStyleSheet('color: %s;' % ('green' if self.incoming else 'red'))
         layout.addWidget(
-            QLabel('Amount: %f' % self.transaction.amount)
+            amount_label
         )
         layout.addWidget(
             QLabel('Timestamp: %s' % self.transaction.timestamp)
@@ -30,18 +33,38 @@ class TransactionWidget(QWidget):
         
         
 class TransactionView(QScrollArea):
-    def __init__(self, transactions: list[Transaction], parent: QObject | None = None) -> None:
+    def __init__(self, incoming_transactions: list[Transaction], outcoming_transactions: list[Transaction], parent: QObject | None = None) -> None:
         super(TransactionView, self).__init__(parent)
-        self.transactions = transactions
+        self.incoming_transactions = incoming_transactions
+        self.outcoming_transactions = outcoming_transactions
         self._setup_layout()
-        
+    
     def _setup_layout(self) -> None:
         widget = QWidget(self)
         layout = QVBoxLayout(widget)
-        for transaction in self.transactions:
-            layout.addWidget(
-                TransactionWidget(transaction=transaction, parent=widget)
+        transaction_widgets: list[TransactionWidget] = []
+        for transaction in self.incoming_transactions:
+            transaction_widgets.append(
+                TransactionWidget(
+                    transaction=transaction,
+                    incoming=True,
+                    parent=widget
+                )
             )
+        for transaction in self.outcoming_transactions:
+            transaction_widgets.append(
+                TransactionWidget(
+                    transaction=transaction,
+                    incoming=False,
+                    parent=widget
+                )
+            )
+        transaction_widgets = sorted(
+            transaction_widgets,
+            key=lambda widget: int(widget.transaction.timestamp)
+        )
+        for transaction_widget in transaction_widgets:
+            layout.addWidget(transaction_widget)
         self.setWidget(
             widget
         )
